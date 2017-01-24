59b7b19c2dc0e4cb0df19ae138c5306e6e89bffe
Tris <beatricebock_js@hotmail.com>
2017-01-24 11:18:36 +0800

    added database related methods

--- SDPAssignment/Register.aspx.vb
+++ SDPAssignment/Register.aspx.vb
@@ -2,6 +2,30 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim con As New OleDb.OleDbConnection
+
+        Dim dbProvider As String
+        Dim dbSource As String
+        Dim sql As String
+
+        dbProvider = "PROVIDER = MICROSOFT.ACE.OLEDB.12.0"
+        dbSource = "Data Source=|Data Directory|\OrientHotel.mdb"
+
+        con.ConnectionString = dbProvider & dbSource
+
+        con.Open()
+
+        Dim ds As New DataSet
+        Dim da As New OleDb.OleDbDataAdapter
+
+        sql = "SELECT * FROM OrientHotel "
+        da = New OleDb.OleDbDataAdapter(sql, con)
+        da.Fill(ds, "Customer")
+
+        FName.Text = ds.Tables("CustFName").Rows(0).Item(1)
+    End Sub
+
+    Protected Sub CreateUser_Click(sender As Object, e As EventArgs)
 
     End Sub
 
